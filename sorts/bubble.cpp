b960d766410c07f75825997e29f5767aa12ec90c
Martin weiss <Martin.weiss2410@gmail.com>
2026-07-21 14:15:30 +0700

    added a bubble sort example in /sort

---
+++ sorts/bubble.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int n, vector<int>& v ){
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n - i - 1; j ++) {
+			if(v[j] < v[j + 1]) {
+				int temp = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = temp;
+			}
+		}
+	}
+} 
+
+int main() {
+	
+	vector<int> vec = {12, 23, 19, 1, 12, 11, 9, 2, 3};
+
+	bubbleSort(vec.size(), vec);
+	for (int x: vec) {
+		cout << x << " ";
+	}
+}
--- sorts/insertion.cpp
+++ sorts/insertion.cpp
@@ -21,77 +21,14 @@ void insertionSort(vector<int>& arr) {
 	}
 }
 
-// this uses recusion 
-void mergesort(int arr[], int start, int end) {
-	
-	// if sub arr has only 1 element then it is sorted	
-	if (start == end) {
-		return;
-	}
-	
-	// find midpoint
-	int mid = (start + end) / 2;
-
-	// recursively sort each part
-	mergesort(arr, start, mid);
-	mergesort(arr, mid+1, end);
-	
-	// create temp arr
-	int c[end - start + 1];
-	
-	// init pointers
-	int ia = start, ib = mid + 1, ic = 0;
-	
-	// merge everything back
-	while(ia <= mid && ib <= end){
-
-    		if(arr[ia] <= arr[ib]){
-        		c[ic] = arr[ia];
-        		ia++;
-        		ic++;
-    		}else{
-        		c[ic] = arr[ib];
-        		ib++;
-        		ic++;
-   		}
-
-	}	
-	
-	while (ia<=mid) {
-		c[ic] = arr[ia];
-		ia ++;
-		ic ++;
-	}
-
-	while (ib<=end) {
-		c[ic] = arr[ib];
-		ib ++;
-		ic ++;
-	}
-
-	for (int i = 0; i < ic; i++) {
- 		arr[start + i] = c[i];
-	}
-
-
-}
-
 
 int main() {
 	vector<int> a = {5,3,8,4,2};
 	insertionSort(a);
 
-	int arr[] = {5, 3, 8, 4, 2, 1};
-
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	mergesort(arr, 0, n - 1);
-
 	for (int x: a) {
 		cout << x << " ";
 	}
 
-	for (int x: arr) {
-		cout << x << " "; 
-	}
+
 }

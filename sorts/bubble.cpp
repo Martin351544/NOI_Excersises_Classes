74772ded704c61d4b32192f06747b4ef13d890f3
Martin weiss <Martin.weiss2410@gmail.com>
2026-07-22 08:22:17 +0700

    Edited the sorts (added comments for bubble sort)

--- sorts/bubble.cpp
+++ sorts/bubble.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 void bubbleSort(int n, vector<int>& v ){
-	
+	// first outer loop	
 	for (int i = 0; i < n; i++) {
+		// second inner loop
 		for (int j = 0; j < n - i - 1; j ++) {
+			// comparison
 			if(v[j] < v[j + 1]) {
 				int temp = v[j];
 				v[j] = v[j + 1];
